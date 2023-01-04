0f69327ed457355b15456ee9db4da5021510e778
zhangqi <zhangqi13c@163.com>
2023-01-04 15:47:31 +0800

    Add knife4j support

--- src/main/java/com/joe/doc/config/SwaggerConfig.java
+++ src/main/java/com/joe/doc/config/SwaggerConfig.java
@@ -1,12 +1,20 @@
 package com.joe.doc.config;
 
 import cn.hutool.core.util.RandomUtil;
+import io.swagger.v3.oas.annotations.enums.ParameterIn;
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.media.StringSchema;
+import io.swagger.v3.oas.models.parameters.Parameter;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.customizers.GlobalOpenApiCustomizer;
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.HandlerMethod;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -41,10 +49,32 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenApi() {
-        Info info = new Info().title("文档管理系统API").version("1.0")
-                .description("文档管理")
+        Info info = new Info().title("文档管理系统API").version("1.0").description("文档管理")
                 .license(new License().name("Apache 2.0"));
         return new OpenAPI().info(info);
+    }
+
+    @Bean
+    public OperationCustomizer customGlobalHeaders() {
+        return (Operation operation, HandlerMethod handlerMethod) -> {
+            Parameter missingParam1 = new Parameter()
+                    .in(ParameterIn.HEADER.toString())
+                    .schema(new StringSchema())
+                    .name("missingParam1")
+                    .description("header description2")
+                    .required(true);
+
+            Parameter missingParam2 = new Parameter()
+                    .in(ParameterIn.HEADER.toString())
+                    .schema(new StringSchema())
+                    .name("missingParam2")
+                    .description("header description2")
+                    .required(true);
+
+            operation.addParametersItem(missingParam1);
+            operation.addParametersItem(missingParam2);
+            return operation;
+        };
     }
 
 }

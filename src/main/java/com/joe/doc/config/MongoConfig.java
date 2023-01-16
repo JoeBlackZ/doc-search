36b2ffa74c63fccbed85c9f4ba78631c10045ab0
zhangqi <zhangqi13c@163.com>
2023-01-16 19:08:35 +0800

    Add elasticsearch-cluster.yml and elasticsearch-standalone.yml

--- src/main/java/com/joe/doc/config/MongoConfig.java
+++ src/main/java/com/joe/doc/config/MongoConfig.java
@@ -1,17 +1,7 @@
 package com.joe.doc.config;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.NoSuchBeanDefinitionException;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.convert.CustomConversions;
-import org.springframework.data.mongodb.MongoDatabaseFactory;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
-import org.springframework.data.mongodb.core.convert.DbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultDbRefResolver;
-import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
-import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
-import org.springframework.data.mongodb.core.mapping.MongoMappingContext;
 
 /**
  * @description Mongodb config
